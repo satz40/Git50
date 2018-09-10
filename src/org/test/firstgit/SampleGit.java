de0ab89aadf6a3708bbd7dd8cdeceaa397918f5f
vicz95 <mail2vignesh95@gmail.com>
2018-09-10 12:46:49 +0530

    method added

--- src/org/test/firstgit/SampleGit.java
+++ src/org/test/firstgit/SampleGit.java
@@ -2,7 +2,12 @@ package org.test.firstgit;
 
 public class SampleGit {
 
+public void empNmae() {
+System.out.println("jana");
+}
 	public static void main(String[] args) {
+		SampleGit f=new SampleGit();
+		f.empNmae();
 		System.out.println("My Program");
 	}
 
